abf54c53d9852f1564c529bb31f0cf72cc374bd5
Eugene Ahn <01.ahn.eugene@gmail.com>
2019-12-07 11:30:58 -0800

    Add Partners

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -7,7 +7,10 @@ function About(props) {
 
     const partners = props.partners.map(partner => {
         return (
-            <h5>{partner.name}</h5>
+           // <h5>{partner.name}</h5>
+           <Media tag ="li" key= {partner.id} > 
+                 <RenderPartner partner={partner}  /> 
+           </Media>
         );
     });
 
@@ -71,6 +74,31 @@ function About(props) {
             </div>
         </div>
     );
+}
+
+function RenderPartner({partner}) {
+        if(partner){
+            return (
+                <React.Fragment>
+                    < Media object src={partner.image} alt={partner.name} width= "150" />
+                            <Media body className = "ml-5 mb-4"> 
+                                <Media heading> 
+                                    {partner.name}
+                                </Media>
+                                {partner.description}   
+                           </Media>
+                        
+                </React.Fragment>
+
+            ); 
+
+
+
+        }
+
+        return 
+
+
 }
 
 export default About;
