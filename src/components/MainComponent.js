28725128f6af6a9c8fe01159bc0a84d13c594b38
Eugene Ahn <01.ahn.eugene@gmail.com>
2019-12-14 08:04:19 -0800

    React Redux Form Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,20 +6,22 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux'; 
 
-const mapstateToProps = state => {
-    return{
-        campsite: state.campsites,
-        comments: state.comments, 
-        partners: state.partners, 
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites,
+        comments: state.comments,
+        partners: state.partners,
         promotions: state.promotions
-    }
-}
+    };
+};
+
 class Main extends Component {
 
     render() {
+
         const HomePage = () => {
             return (
                 <Home
@@ -27,17 +29,13 @@ class Main extends Component {
                     promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
                     partner={this.props.partners.filter(partner => partner.featured)[0]}
                 />
-            )
-        }
+            );
+        };
 
-        const CampsiteWithId = ({ match }) => {
+        const CampsiteWithId = ({match}) => {
             return (
-                <CampsiteInfo
-                    campsite={this.props.campsites.filter(campsite => campsite.id ===
-                        +match.params.campsiteId)[0]}
-                    comments={this.props.comments.filter(comment => comment.campsiteId ===
-                        +match.params.campsiteId)}
-                />
+                <CampsiteInfo campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
+                  comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} />
             );
         };
 
@@ -49,7 +47,7 @@ class Main extends Component {
                     <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
-                    <Route exact path='/aboutus' render={() => <About partners={this.props.partners} />} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.props.partners} /> } />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
@@ -57,4 +55,5 @@ class Main extends Component {
         );
     }
 }
+
 export default withRouter(connect(mapStateToProps)(Main));
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,11 @@
-import {createStore} from 'redux'; 
-import {Reducer, initialState} from './reducer.';
+import { createStore } from 'redux';
+import { Reducer, initialState } from './reducer';
 
-export const ConfigureStore = () =>{
+export const ConfigureStore = () => {
     const store = createStore(
-        Reducer, 
+        Reducer,
         initialState
+    );
 
-    )
-    return store; 
-}
+    return store;
+};
